b848bc6d0ed032121c2bcf5a54c0eb1adf329f5d
nayemshaon34 <nayemshaon34@gmail.com>
2021-09-17 18:27:13 +0600

    done

--- js/country.js
+++ js/country.js
@@ -3,7 +3,7 @@ const loadData = () => {
     .then(response => response.json())  //converting into json
     .then(data => getData(data));
 }
-
+loadData(); //calling the function
 const getData = countries =>{
     const list = document.getElementById('list');
     countries.forEach(country => {
@@ -12,10 +12,24 @@ const getData = countries =>{
     div.classList.add("country"); //class add
     div.innerHTML = `<h3>${country.name}</h3>
     <h4>${country.capital}</h4>
-    <button>Details</button>
+    <button onclick="loadCountryByName('${country.name}')">Details</button>
     `;
     list.appendChild(div);
     }); 
+}
+
+const loadCountryByName = name => {
+    console.log(name);
+    fetch(`https://restcountries.eu/rest/v2/name/${name}`)//dynamic er jnno ${} eta use korbo
+    .then(res => res.json())
+    .then(data =>  loadCountryDetails(data[0])); //as it is object in array
     
 }
-loadData(); //calling the function
+    
+const loadCountryDetails = data => {
+    // console.log(data.population);
+    const div = document.getElementById('country-detail');
+    div.innerHTML = ` <h4>${data.name}</h4>
+    <p>Population: ${data.population}</p>
+    <img width="300px" src="${data.flag}" alt="">`;    
+}
